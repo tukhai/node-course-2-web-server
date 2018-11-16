b941153954708bb556ab80b55d1a1039d4ac9e1d
Admin <admin@admins-mbp.edgepropsg.local>
2018-11-16 16:23:12 +0800

    Add project page

--- server.js
+++ server.js
@@ -58,6 +58,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects',
+        projectMessage: 'Portfolio Project'
+    });
+});
+
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
     res.send({
